a19a908efb200c822c88873bb23f29a01f8370f5
dunklesToast <tom.sacher@mcggehrden.de>
2018-07-03 22:25:28 +0200

    required rdbdash

--- database/import.js
+++ database/import.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const r = require('rethinkdbdash');
 
 async function load() {
     await r.db('facemash2').table('data').insert(JSON.parse(fs.readFileSync('./data.json.json').toString())).run();
